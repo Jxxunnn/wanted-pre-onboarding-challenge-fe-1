35bca9fb6e45ff7a49809d5832a2c0e3880bb299
Jxxunnn <roof2586@naver.com>
2023-01-08 22:18:17 +0900

    feat: LoginPage 컴포넌트 생성

---
+++ front/src/page/LoginPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Form from "../../components/login/Form";
+
+export default function LoginPage({ type }) {
+  return (
+    <section>
+      <h1>인증 페이지</h1>
+      <Form type={type} />
+      {type === "login" ? <Link to="signUp">회원가입</Link> : null}
+    </section>
+  );
+}
