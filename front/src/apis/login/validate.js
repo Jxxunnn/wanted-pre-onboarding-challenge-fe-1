d286c2363cf38e1d68f1e3d35df233099e5cb5b0
Jxxunnn <roof2586@naver.com>
2023-01-08 22:16:32 +0900

    feat: login validation 함수 구현

---
+++ front/src/apis/login/validate.js
@@ -0,0 +1,36 @@
+import API from "../api";
+import { getToken, setToken } from "../../utils/handleToken";
+
+const validate = async (state = {}, type) => {
+  let auth;
+
+  if (type === "login") {
+    try {
+      auth = await API.post("/users/login", {
+        email: state.id,
+        password: state.password,
+        header: {
+          Authorization: getToken(),
+        },
+      });
+    } catch (error) {
+      throw new Error("로그인 인증 실패");
+    }
+  }
+  if (type === "signUp") {
+    try {
+      auth = await API.post("/users/create", {
+        email: state.id,
+        password: state.password,
+      });
+
+      setToken(auth.data.token);
+    } catch (error) {
+      throw new Error("회원가입 인증 실패");
+    }
+  }
+
+  return auth;
+};
+
+export default validate;
