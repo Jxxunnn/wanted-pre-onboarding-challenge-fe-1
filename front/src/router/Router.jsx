e9cf33b7122e5e92560ff8b17b9bae8e32d594d4
Jxxunnn <roof2586@naver.com>
2023-01-08 22:15:26 +0900

    feat: router 분리

---
+++ front/src/router/Router.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import LoginPage from "../page/LoginPage/index";
+import HomePage from "../page/HomePage";
+import { hasToken } from "../utils/handleToken";
+
+export default function Router() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (hasToken()) {
+      navigate("/");
+    } else {
+      navigate("/auth");
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth" element={<LoginPage type="login" />} />
+      <Route path="/auth/signUp" element={<LoginPage type="signUp" />} />
+    </Routes>
+  );
+}
