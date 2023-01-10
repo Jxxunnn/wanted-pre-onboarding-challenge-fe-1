ee42bba36f66bdbc8702d8f704909400792148d2
Jxxunnn <roof2586@naver.com>
2023-01-10 18:11:51 +0900

    feat: removeToken 함수 추가

--- client/src/utils/handleToken.js
+++ client/src/utils/handleToken.js
@@ -1,5 +1,6 @@
 const getToken = () => localStorage.getItem("token");
 const setToken = (token) => localStorage.setItem("token", token);
 const hasToken = () => !!localStorage.token;
+const removeToken = () => localStorage.removeItem("token");
 
-export { getToken, setToken, hasToken };
+export { getToken, setToken, hasToken, removeToken };
