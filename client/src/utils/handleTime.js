a9a84d32e3360e2c2ad79ea6b8d38cb29a8b8b54
Jxxunnn <roof2586@naver.com>
2023-01-10 19:42:15 +0900

    feat: date parsing 함수 개발

---
+++ client/src/utils/handleTime.js
@@ -0,0 +1,11 @@
+const parseTime = (format) => {
+  if (!format) return null;
+  const timestamp = Date.parse(format);
+  const date = new Date(timestamp);
+
+  return `${date.getFullYear()}/${
+    date.getMonth() + 1
+  }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+export default parseTime;
