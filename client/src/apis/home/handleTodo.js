ae7e19e3175921ff982080b7afa5f1bb6b2c7b59
Jxxunnn <roof2586@naver.com>
2023-01-10 17:30:40 +0900

    fix: Todo List API 함수 수정

--- client/src/apis/home/handleTodo.js
+++ client/src/apis/home/handleTodo.js
@@ -1,45 +1,81 @@
 import API from "../api";
 import { getToken } from "../../utils/handleToken";
 
-const createTodo = async (state = {}, setState = () => {}) => {
-  API.post(
-    "/todos",
-    {
-      title: state.title,
-      content: state.content,
-    },
-    {
+const createTodo = async (state = {}, setState = (nextState) => {}) => {
+  try {
+    await API.post(
+      "/todos",
+      {
+        title: state.title,
+        content: state.content,
+      },
+      {
+        headers: {
+          Authorization: getToken(),
+        },
+      },
+    );
+
+    getTodos(setState);
+  } catch (error) {
+    throw new Error("투두 생성을 실패했습니다.");
+  }
+};
+const deleteTodo = async (id, setState) => {
+  try {
+    await API.delete(`/todos/${id}`, {
       headers: {
         Authorization: getToken(),
       },
-    },
-  );
+    });
+
+    getTodos(setState);
+  } catch (error) {
+    throw new Error("투두 삭제를 실패했습니다.");
+  }
 };
-const deleteTodo = async (id) => {
-  API.delete(`/todos/${id}`, {
-    headers: {
-      Authorization: getToken(),
-    },
-  });
-};
-const updateTodo = async (state = {}) => {
-  API.put(
-    `/todos/${state.id}`,
-    {
-      title: state.title,
-      content: state.content,
-    },
-    {
-      headers: {
-        Authorization: getToken(),
+const updateTodo = async (state = {}, id, setState = (nextState) => {}) => {
+  try {
+    await API.put(
+      `/todos/${id}`,
+      {
+        title: state.title,
+        content: state.content,
       },
-    },
-  );
+      {
+        headers: {
+          Authorization: getToken(),
+        },
+      },
+    );
+
+    getTodos(setState);
+  } catch (error) {
+    throw new Error("투두 업데이트를 실패했습니다.");
+  }
 };
 
 const getTodo = async (id, setState = (nextState) => {}) => {
   try {
-    const res = await API.get(`/todos/${id}`);
+    const res = await API.get(`/todos/${id}`, {
+      headers: {
+        Authorization: getToken(),
+      },
+    });
+
+    setState(res.data.data);
+  } catch (error) {
+    throw new Error("투두 가져오기를 실패했습니다.");
+  }
+};
+
+const getTodos = async (setState = () => {}) => {
+  try {
+    const res = await API.get("/todos", {
+      headers: {
+        Authorization: getToken(),
+      },
+    });
 
     setState(res.data.data);
   } catch (error) {
